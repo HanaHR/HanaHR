19e9c35a29a5c4d894f9f61acba5332c808a59c1
Minkyung <lanikang.319@gmail.com>
2023-06-19 09:08:03 +0900

    db, api, rq 파일 생성

---
+++ src/com/hanaHR/web/DB1.java
@@ -0,0 +1,26 @@
+package com.hanaHR.web;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB1 {
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/JDBC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1234";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection(JDBC_URL, USERNAME, PASSWORD);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
---
+++ src/com/hanaHR/web/EmailUtils.java
@@ -0,0 +1,54 @@
+package com.hanaHR.web;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailUtils {
+
+    public static void sendEmail(String recipient, String subject, String content) {
+        // 발신자 계정 정보 설정
+        String senderEmail = "lanikang.319@gmail.com"; // 발신자 이메일 주소
+        String senderPassword = "H*kmksk0301!"; // 발신자 이메일 계정 비밀번호
+
+        // SMTP 서버 설정
+        String smtpHost = "smtp.gmail.com"; // SMTP 서버 호스트
+        String smtpPort = "587"; // SMTP 서버 포트 (일반적으로 587 또는 465)
+
+        // 메일 속성 설정
+        Properties properties = new Properties();
+        properties.put("mail.smtp.host", smtpHost);
+        properties.put("mail.smtp.port", smtpPort);
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+
+        // 인증 정보 설정
+        Authenticator authenticator = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(senderEmail, senderPassword);
+            }
+        };
+
+        // 메일 세션 생성
+        Session session = Session.getInstance(properties, authenticator);
+
+        try {
+            // 메시지 생성
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(senderEmail));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+            message.setSubject(subject);
+            message.setText(content);
+
+            // 메일 전송
+            Transport.send(message);
+
+            System.out.println("이메일이 성공적으로 전송되었습니다.");
+        } catch (MessagingException e) {
+            System.out.println("이메일 전송 중 오류가 발생했습니다.");
+            e.printStackTrace();
+        }
+    }
+}
